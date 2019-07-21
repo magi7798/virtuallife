6556cbff1b3bffe7866b8bd9898b9d923b9a6628
magi7798 <48306858+magi7798@users.noreply.github.com>
2019-07-20 22:23:26 -0400

    fixed index.js (#1)

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import reducers from './reducers';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
+console.log("Test");
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
